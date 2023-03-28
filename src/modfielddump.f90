40c2edfb509635ff56a447ed83a6309c041e848d
Alessandro Savazzi <ale.savazzi95@gmail.com>
2023-03-28 16:45:00 +0200

    Added pressure perturbation as an output in fielddup

--- src/modfielddump.f90
+++ src/modfielddump.f90
@@ -34,7 +34,7 @@ private
 PUBLIC :: initfielddump, fielddump,exitfielddump
 save
 !NetCDF variables
-  integer :: nvar = 7
+  integer :: nvar = 8
   integer :: ncid,nrec = 0
   character(80) :: fname = 'fielddump.xxx.xxx.xxx.nc'
   character(80),dimension(:,:), allocatable :: ncname
@@ -112,6 +112,7 @@ contains
       call ncinfo(ncname( 6,:),'thl','Liquid water potential temperature above 300K','K','tttt')
 !       call ncinfo(ncname( 7,:),'qr','Rain water mixing ratio','1e-5kg/kg','tttt')
       call ncinfo(ncname( 7,:),'buoy','Buoyancy','K','tttt')
+      call ncinfo(ncname( 8,:),'p','Pressure perturbation from poisson solver','Pa','tttt')
       do n=1,nsv
         write (csvname(1:3),'(i3.3)') n
         call ncinfo(ncname(7+n,:),'sv'//csvname,'Scalar '//csvname//' specific concentration','(kg/kg)','tttt')
@@ -135,6 +136,7 @@ contains
     use modmpi,    only : myid,cmyidx, cmyidy
     use modstat_nc, only : lnetcdf, writestat_nc
     use modmicrodata, only : iqr, imicro, imicro_none
+    use modpois, only : p
     implicit none
 
     integer(KIND=selected_int_kind(4)), allocatable :: field(:,:,:)
@@ -238,6 +240,19 @@ contains
       close (ifoutput)
     end if
 
+    field = NINT(1.0E5*p,2)
+    if (lnetcdf) vars(:,:,:,8) = p(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
+    if (lbinary) then
+      if (ldiracc) then
+        open (ifoutput,file='wbpp.'//cmyidx//'.'//cmyidy//'.'//cexpnr,access='direct', form='unformatted', recl=reclength)
+        write (ifoutput, rec=writecounter) field(2:i1:ncoarse,2:j1:ncoarse,klow:khigh)
+      else
+        open  (ifoutput,file='wbpp.'//cmyidx//'.'//cmyidy//'.'//cexpnr,form='unformatted',position='append')
+        write (ifoutput) (((field(i,j,k),i=2,i1, ncoarse),j=2,j1, ncoarse),k=klow,khigh)
+      end if
+      close (ifoutput)
+    end if
+
     if(imicro/=imicro_none) then
       do i=2-ih,i1+ih
       do j=2-jh,j1+jh
@@ -294,7 +309,7 @@ contains
       close (ifoutput)
     endif
 
-    if (lnetcdf) vars(:,:,:,8:nvar) = sv0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh,:)
+    if (lnetcdf) vars(:,:,:,9:nvar) = sv0(2:i1:ncoarse,2:j1:ncoarse,klow:khigh,:)
 
     if(lnetcdf) then
       call writestat_nc(ncid,1,tncname,(/rtimee/),nrec,.true.)
